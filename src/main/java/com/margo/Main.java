00a0f004dadb97e267fcd141df3646e950a443b2
Rubie De Oliveira <rubiedeoliveira@gmail.com>
2020-07-27 21:58:36 +0200

    update with 2 different formats

--- src/main/java/com/margo/Main.java
+++ src/main/java/com/margo/Main.java
@@ -1,6 +1,5 @@
 package com.margo;
 
-
 import com.margo.AbstractFactory.WriterFactoryOnFile;
 import com.margo.AbstractFactory.WriterFactoryOnStandardOutput;
 import com.margo.Factory.Writer;
@@ -15,23 +14,32 @@ import com.margo.Format.StylizedFormat;
 import java.util.ArrayList;
 
 import static com.margo.Format.Colors.Color.CYAN;
+import static com.margo.Format.Colors.Color.YELLOW;
 import static com.margo.Format.Style.Style.BOLD;
+import static com.margo.Format.Style.Style.UNDERLINE;
 
 public class Main {
 
     public static void main(String[] args) {
-        Format basicFomat = new BasicFormat();
+        Format basicFormat = new BasicFormat();
+        Format basicFormatStd = new BasicFormat();
 
         ArrayList<Color> colors = new ArrayList<Color>() {{ add(CYAN); }};
-        Format coloredFormat = new ColoredFormat(colors, basicFomat);
-
+        Format coloredFormat = new ColoredFormat(colors, basicFormat);
         ArrayList<Style> styles = new ArrayList<Style>() {{ add(BOLD); }};
         Format format = new StylizedFormat(styles, coloredFormat);
 
+        ArrayList<Color> colorsStd = new ArrayList<Color>() {{ add(YELLOW); }};
+        Format coloredFormatStd = new ColoredFormat(colorsStd, basicFormatStd);
+        ArrayList<Style> stylesStd = new ArrayList<Style>() {{ add(UNDERLINE); }};
+        Format formatStd = new StylizedFormat(stylesStd, coloredFormatStd);
+
+
         Writer writerOnStandardOutput = WriterFactory.getWriter(
-                new WriterFactoryOnStandardOutput(format));
+                new WriterFactoryOnStandardOutput(formatStd));
         Writer writerOnFile = WriterFactory.getWriter(
                 new WriterFactoryOnFile(format, "file1"));
+
 
         String textToWrite = "Hello World !";
         writerOnFile.write(textToWrite);
